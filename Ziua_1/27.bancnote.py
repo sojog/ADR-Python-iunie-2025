d5271c828af47ce490841ef2d540823a3595203b
Silviu Ojog <sil.ojog@gmail.com>
2025-06-19 14:39:43 +0300

    Exercitiu bancnote - starter

---
+++ Ziua_1/27.bancnote.py
@@ -0,0 +1,4 @@
+# Creaţi un program în care utilizatorul va insera o sumă întreagă.
+# Programul trebuie să calculeze şi să afişeze numărul corespunzător de bancnote, unde în program sunt definite bancnotele cu valorile: 10, 5, 2 şi 1
+
+# 128 -> 12 bancnote de 5, 1 de 5, 1 de 2, si 1 de 1
